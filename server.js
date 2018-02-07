2a21de66df17f80294a38987a1f794b4fb94ef3e
Ruha Peera <ruha.rizwan@gmail.com>
2018-02-06 22:57:17 -0500

    adds scrape route

--- server.js
+++ server.js
@@ -37,43 +37,96 @@ mongoose.connect(MONGODB_URI, {
 });
 
 //ROUTES
+app.get("/scrape", function(req, res) {
+	//Making the request 
+	request("http://www.bbc.co.uk/food/cuisines", function(error, response, html) {
 
-//GET route that will dhow that scrape is complete
+		//Loading the html
+		var $ = cheerio.load(html);
 
-//Making the request 
-request("http://www.bbc.co.uk/food/cuisines", function(error, response, html) {
+		//Empty array to save scraped data
+		// var results = [];
+	 	
+		//targeting the li element
+		$("li.cuisine").each(function(i, element) {
 
-	//Loading the html
-	var $ = cheerio.load(html);
+			var results = {};
 
-	//Empty array to save scraped data
-	var results = [];
- 	
-	//targeting the li element
-	$("li.cuisine").each(function(i, element) {
+			//saving the cuisine-title text in title variable
+			results.title = $(this).find("h3").find("a").find("span").text();
 
-		//saving the cuisine-title text in title variable
-		var title = $(element).find("h3").find("a").find("span").text();
+			//saving the link to the link variable
+			results.link = $(this).find("h3").find("a").attr("href");
 
-		//saving the link to the link variable
-		var link = $(element).find("h3").find("a").attr("href");
+			//saving the summary to the summary variable
+			results.summary = $(this).find("p").text();
 
-		//saving the summary to the summary variable
-		var summary = $(element).find("p").text();
+			//saving the results into the array
+			// results.push({
+			// 	title: title,
+			// 	link: "www.bbc.co.uk"+link,
+			// 	summary: summary
+			// });
 
-		//saving the results into the array
-		results.push({
-			title: title,
-			link: "www.bbc.co.uk"+link,
-			summary: summary
+			// Create a new Cuisine using the `result` object built from scraping
+	        db.Cuisine.create(results)
+	        .then(function(dbCuisine) {
+	          // View the added result in the console
+	          console.log(dbCuisine);
+	        })
+	        .catch(function(err) {
+	          // If an error occurred, send it to the client
+	          return res.json(err);
+	        });
+
+
+		//Logging the results
+		console.log(results);
+
 		});
 
+		res.send("Scrape Complete");
+		
 	});
 
-	//Logging the results
-	console.log(results);
-
 });
+
+// //Making the request 
+// request("http://www.bbc.co.uk/food/cuisines", function(error, response, html) {
+
+// 	//Loading the html
+// 	var $ = cheerio.load(html);
+
+// 	//Empty array to save scraped data
+// 	var results = [];
+ 	
+// 	//targeting the li element
+// 	$("li.cuisine").each(function(i, element) {
+
+// 		//saving the cuisine-title text in title variable
+// 		var title = $(element).find("h3").find("a").find("span").text();
+
+// 		//saving the link to the link variable
+// 		var link = $(element).find("h3").find("a").attr("href");
+
+// 		//saving the summary to the summary variable
+// 		var summary = $(element).find("p").text();
+
+// 		//saving the results into the array
+// 		results.push({
+// 			title: title,
+// 			link: "www.bbc.co.uk"+link,
+// 			summary: summary
+// 		});
+
+// 	});
+
+// 	//Logging the results
+// 	console.log(results);
+	
+// });
+
+
 
 //Starting the server
 app.listen(PORT, function() {
