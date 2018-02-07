21a9b3b1a55c38fd3981243d01e486bf7b8809ff
Ruha Peera <ruha.rizwan@gmail.com>
2018-02-06 21:42:18 -0500

    resolves bug that wouldnt recognise morgan and mongoose

--- server.js
+++ server.js
@@ -2,22 +2,39 @@
 var express = require("express");
 var mongojs = require("mongojs");
 var bodyParser = require("body-parser");
-var mongoose = require("morgan");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+//scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
 
-//Initiaizing express
+//Initializing express
 var app = express();
+var PORT = 8000;
+
+//Requiring models
+var db = require("./models");
+
+//Middleware
+//For loggin requests
+app.use(logger("dev"));
+
+//For handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Static directory
+app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-// mongoose.Promise = Promise;
-// mongoose.connect(MONGODB_URI, {
-//   useMongoClient: true
-// });
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+	//CLI told me to remove the line below
+  	// useMongoClient: true
+});
 
 //Making the request 
 request("http://www.bbc.co.uk/food/cuisines", function(error, response, html) {
@@ -27,27 +44,18 @@ request("http://www.bbc.co.uk/food/cuisines", function(error, response, html) {
 
 	//Empty array to save scraped data
 	var results = [];
-
-	// //Find each span, with "cuisine-title" class
-	// $("span.cuisine-title").each(function(i, element) {
-
-	// 	//saving the cuisine-title text in title variable
-	// 	var title = $(element).text();
-
-	// 	//targeting parent element of href
-	// 	var link = $(element).parent().attr("href");
-
-	// 	
-
+ 	
+	//targeting the li element
 	$("li.cuisine").each(function(i, element) {
 
-	//saving the cuisine-title text in title variable
-	var title = $(element).find("h3").find("a").find("span").text();
+		//saving the cuisine-title text in title variable
+		var title = $(element).find("h3").find("a").find("span").text();
 
-	var link = $(element).find("h3").find("a").attr("href");
+		//saving the link to the link variable
+		var link = $(element).find("h3").find("a").attr("href");
 
-	var summary = $(element).find("p").text();
-
+		//saving the summary to the summary variable
+		var summary = $(element).find("p").text();
 
 		//saving the results into the array
 		results.push({
@@ -55,12 +63,15 @@ request("http://www.bbc.co.uk/food/cuisines", function(error, response, html) {
 			link: "www.bbc.co.uk"+link,
 			summary: summary
 		});
+
 	});
-
-
-	
 
 	//Logging the results
 	console.log(results);
 
+});
+
+//Starting the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
